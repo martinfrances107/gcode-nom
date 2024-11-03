102674db57598a06d20975889e47ca7566bf5535
martinfrances107 <martinfrances107@hotmail.com>
2024-11-03 18:33:54 +0000

    Obj: Fixed stringy issues
    When the E(xtruder) - is zero the head is moving without printing and the
    visualiser should start a new line.

--- gcode2obj/src/obj.rs
+++ gcode2obj/src/obj.rs
@@ -1,4 +1,5 @@
 use core::fmt::Display;
+use core::mem;
 use std::collections::HashMap;
 use std::hash::Hash;
 use std::hash::Hasher;
@@ -8,10 +9,10 @@ use gcode_nom::parms::PosVal;
 use gcode_nom::CoordPos;
 
 #[derive(Debug, Clone)]
-struct VERTEX(f64, f64, f64);
+struct Vertex(f64, f64, f64);
 
-impl Eq for VERTEX {}
-impl PartialEq for VERTEX {
+impl Eq for Vertex {}
+impl PartialEq for Vertex {
     fn eq(&self, other: &Self) -> bool {
         self.0.to_bits() == other.0.to_bits()
             && self.1.to_bits() == other.1.to_bits()
@@ -19,11 +20,11 @@ impl PartialEq for VERTEX {
     }
 }
 
-impl Hash for VERTEX {
+impl Hash for Vertex {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.0.to_bits().hash(state);
         self.1.to_bits().hash(state);
-        self.2.to_bits().hash(state)
+        self.2.to_bits().hash(state);
     }
 }
 
@@ -32,10 +33,12 @@ impl Hash for VERTEX {
 pub struct Obj {
     /// De-duplicating structure.
     //   Given a point return the postion in the vertex_buffer
-    index_store: HashMap<VERTEX, usize>,
-    vertex_buffer: Vec<VERTEX>,
+    index_store: HashMap<Vertex, usize>,
+    vertex_buffer: Vec<Vertex>,
 
-    index_buffer: Vec<usize>,
+    // A collection of lines
+    // A collection index_buffers reperesenting line.
+    lines: Vec<Vec<usize>>,
 }
 
 // A line could not be decoded as an G-Code command
@@ -53,16 +56,18 @@ impl Display for Obj {
         // Output vertex buffer
         // "List of geometric vertices, with (x, y, z, [w]) coordinates, w is optional and defaults to 1.0."
         // [spec](<https://en.wikipedia.org/wiki/Wavefront_.obj_file>)
-        for VERTEX(x, y, z) in &self.vertex_buffer {
-            println!("v {} {} {}", x, y, z);
+        for Vertex(x, y, z) in &self.vertex_buffer {
+            writeln!(f, "v {x} {y} {z}")?;
         }
 
-        // l 1 2 3  as list of vertex indicies.
-        // '+1' convert from zero based counting.
-        // The first index is '1'.
-        print!("l");
-        for i in &self.index_buffer {
-            print!(" {}", i + 1);
+        for line in &self.lines {
+            // line "l 1 2 3"  list of vertex indicies.
+            write!(f, "l")?;
+            for i in line {
+                // '+1' convert from zero based counting.
+                // The first index is '1'.
+                write!(f, " {}", i + 1)?;
+            }
         }
         Ok(())
     }
@@ -77,16 +82,18 @@ impl FromIterator<String> for Obj {
     {
         let mut obj = Self::default();
 
+        let mut is_extruding = true;
         let mut abs_coords = CoordPos::default();
         let mut z = 0_f64;
         let mut next_vertex_pos = 0;
+        let mut line_buffer = vec![];
         for line in iter {
             let (_, command) = Command::parse_line(&line).expect("Command not parseable");
             let mut x = f64::NAN;
             let mut y = f64::NAN;
             match command {
                 // A non printable move.
-                Command::G0(mut payload) => {
+                Command::G0(_) => {
                     todo!();
                 }
                 // A printable move.
@@ -97,8 +104,9 @@ impl FromIterator<String> for Obj {
                             PosVal::X(val) => x = val,
                             PosVal::Y(val) => y = val,
                             PosVal::Z(val) => z = val,
-                            PosVal::E(_) | PosVal::F(_) => {
-                                // silently drop
+                            PosVal::E(val) => is_extruding = val != 0_f64,
+                            PosVal::F(_) => {
+                                // Silently drop feedrate adjustment.
                             }
                             pos_bad => {
                                 eprintln!("Unexpected param seen in Command::G1 {pos_bad:?}");
@@ -112,29 +120,48 @@ impl FromIterator<String> for Obj {
                     // Valid `Command::G1` -  Where X and Y and undefined
                     //
                     // "G1 E2.72551 F1800.00000"
-                    if !x.is_nan() && !y.is_nan() {
-                        let vertex = VERTEX(x, y, z);
-                        match obj.index_store.get(&vertex) {
-                            Some(index) => {
-                                // Push record of exiting vertex to index_buffer.
-                                obj.index_buffer.push(*index);
+                    if !x.is_nan() && !y.is_nan() && is_extruding {
+                        let vertex = Vertex(x, y, z);
+                        if let Some(index) = obj.index_store.get(&vertex) {
+                            // Push record of exiting vertex to index_buffer.
+                            line_buffer.push(*index);
+                        } else {
+                            // New entry in vertex_buffer and index_buffer.
+                            obj.index_store.insert(vertex.clone(), next_vertex_pos);
+                            line_buffer.push(next_vertex_pos);
+                            obj.vertex_buffer.push(vertex);
+                            next_vertex_pos += 1;
+                        }
+                    }
+                }
+                Command::G90 => abs_coords = CoordPos::Absolute,
+                Command::G91 => abs_coords = CoordPos::Relative,
+                Command::G92(mut params) => {
+                    for p in params.drain() {
+                        match p {
+                            PosVal::E(e) => {
+                                // TODO What about mode Abs or relative?
+                                if e == 0_f64 {
+                                    // The extrude rate is going to zero
+                                    // enter MoveMode ..ie not laying down fibre.
+                                    is_extruding = false;
+                                    // For Visualisation we start a new line.
+                                    //
+                                    // TODO: set the capacity of the complete_line
+                                    // to the last good capacity.
+                                    let mut complete_line = vec![];
+                                    mem::swap(&mut line_buffer, &mut complete_line);
+                                    obj.lines.push(complete_line);
+                                }
                             }
-                            None => {
-                                // New entry in vertex_buffer and index_buffer.
-                                obj.index_store.insert(vertex.clone(), next_vertex_pos);
-                                obj.index_buffer.push(next_vertex_pos);
-                                obj.vertex_buffer.push(vertex);
-                                next_vertex_pos = next_vertex_pos + 1;
+                            bad => {
+                                // Dropping unexpected params
+                                eprintln!("G92 unhandled set position code. P{bad:#?}");
                             }
                         }
                     }
                 }
-                // Command::G21 => obj.parts.push("M0,0".to_string()),
-                Command::G90 => abs_coords = CoordPos::Absolute,
-                Command::G91 => abs_coords = CoordPos::Relative,
-                Command::G92(_) | Command::GDrop(_) | Command::MDrop(_) | Command::Nop => {}
-                Command::G21 => { // Dropping home},
-                }
+                Command::GDrop(_) | Command::MDrop(_) | Command::Nop | _ => {}
             }
         }
         obj
