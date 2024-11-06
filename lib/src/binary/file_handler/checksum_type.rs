b5d9b7845b58ec5a929599ced5bcd1d1c5b27004
martinfrances107 <martinfrances107@hotmail.com>
2024-11-06 22:17:39 +0000

    Docs: the purpose of the checksum field.

--- lib/src/binary/file_handler/checksum_type.rs
+++ lib/src/binary/file_handler/checksum_type.rs
@@ -5,6 +5,7 @@ use nom::number::streaming::le_u16;
 use nom::Err;
 use nom::{combinator::map_res, error::Error, IResult};
 
+// Details if a checksum is appended to all blocks structures.
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub(super) enum ChecksumType {
     #[default]
@@ -16,10 +17,10 @@ impl Display for ChecksumType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::None => {
-                write!(f, "0 - No Checksum")
+                write!(f, "0 - Blocks have no checksum")
             }
             Self::CRC32 => {
-                write!(f, "1 - CRC32")
+                write!(f, "1 - A CRC32 checksum is appended to all block")
             }
         }
     }
