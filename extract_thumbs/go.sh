07fd59e4288b68ed00e22fd6a75ce6205a6634e2
martinfrances107 <martinfrances107@hotmail.com>
2025-03-20 20:30:12 +0000

    Minor: Fixed up go scripts.

--- extract_thumbs/go.sh
+++ extract_thumbs/go.sh
@@ -1,5 +1,8 @@
 #time  RUST_BACKTRACE=1 cargo run  --  ../assets/mini_cube_b.bgcode
 rm *.oci
 rm *.png
-time RUST_LOG=trace cargo run --release  --  ../assets/both\ parts.bgcode
+# time RUST_LOG=trace cargo run --release  --  ../assets/both\ parts.bgcode
+
+# Performance: This takes 1m 20s to render!!!!
+cargo run --release -- ../assets/gear-holder-print-in-place_04n_022mm_pla_mk4_6h49m.bgcode
 
--- gcode2svg/go.sh
+++ gcode2svg/go.sh
@@ -4,9 +4,10 @@ time cargo run --release --  < ../assets/3DBenchy.gcode > benchy.svg
 time cargo run --release --  <  ../assets/II\ 2x4_0.15mm_PLA_MK3S_1h17m.gcode  > II.svg
 time cargo run --release --  <  ../assets/O\ 6x6_0.15mm_PLA_MK3S_3h56m.gcode > O.svg
 time cargo run --release --  <  ../assets/T\ 2x3_0.15mm_PLA_MK3S_1h3m.gcode > T.svg
+time cargo run --release --  < ../assets/mini_cube_a.gcode > mini_cube_a.svg
 ## No StdIn, passed filename as an argument.
 time cargo run --release --   ../assets/X\ 6x6_0.15mm_PLA_MK3S_1h55m.gcode > X.svg
-## Binary bgcoode files must be passed in as an argument beacuse they are not utf-8 encoded.
+# Binary bgcode files must be passed in as an argument because they are not utf-8 encoded.
 time cargo run --release -- ../assets/both\ parts.bgcode > both.svg
 time cargo run --release -- ../assets/mini_cube_b.bgcode > mini_cube_b.svg
 time cargo run --release -- ../assets/benchy2-mk4s.bgcode > benchy2-mk4s.svg
