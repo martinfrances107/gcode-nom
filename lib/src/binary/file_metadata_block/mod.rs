fb6707ae10dfeb482bcf59747d44cb62fb959848
martinfrances107 <martinfrances107@hotmail.com>
2025-03-29 12:09:39 +0000

    file_metadata: Separated out checksum verification.

--- lib/src/binary/file_metadata_block/mod.rs
+++ lib/src/binary/file_metadata_block/mod.rs
@@ -16,6 +16,7 @@ use super::{
 use crate::binary::default_params::param_parser;
 use crate::binary::default_params::Param;
 use crate::binary::inflate::decompress_data_block;
+use crate::binary::CompressionType;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct FileMetadataBlock<'a> {
@@ -83,9 +84,7 @@ impl FileMetadataBlock<'_> {
 }
 
 static FILE_METADATA_BLOCK_ID: u16 = 0u16;
-pub fn file_metadata_parser_with_checksum(
-    input: &[u8],
-) -> IResult<&[u8], FileMetadataBlock, BlockError> {
+pub fn file_metadata_parser(input: &[u8]) -> IResult<&[u8], FileMetadataBlock, BlockError> {
     let (after_block_header, header) = preceded(
         verify(le_u16, |block_type| {
             log::debug!(
@@ -120,23 +119,6 @@ pub fn file_metadata_parser_with_checksum(
         })
     })?;
 
-    let param_size = 2;
-    let block_size = header.size_in_bytes() + param_size + header.payload_size_in_bytes();
-    let crc_input = &input[..block_size];
-    let computed_checksum = crc32fast::hash(crc_input);
-
-    log::debug!(
-        "file_metadata checksum 0x{checksum:04x} computed checksum 0x{computed_checksum:04x} "
-    );
-    if checksum == computed_checksum {
-        log::debug!("checksum match");
-    } else {
-        log::error!("failed checksum");
-        return Err(Error(BlockError::Checksum(format!(
-            "file_metadata: Checksum mismatch: expected 0x{checksum:04x} got 0x{computed_checksum:04x}"
-        ))));
-    }
-
     Ok((
         after_checksum,
         FileMetadataBlock {
@@ -146,4 +128,34 @@ pub fn file_metadata_parser_with_checksum(
             checksum: Some(checksum),
         },
     ))
+}
+
+pub fn file_metadata_parser_with_checksum(
+    input: &[u8],
+) -> IResult<&[u8], FileMetadataBlock, BlockError> {
+    let (remain, fm) = file_metadata_parser(input)?;
+    if let Some(checksum) = fm.checksum {
+        let param_size = 2;
+        let payload_size = match fm.header.compression_type {
+            CompressionType::None => fm.header.uncompressed_size as usize,
+            _ => fm.header.compressed_size.unwrap() as usize,
+        };
+        let block_size = fm.header.size_in_bytes() + param_size + payload_size;
+        let crc_input = &input[..block_size];
+        let computed_checksum = crc32fast::hash(crc_input);
+
+        log::debug!(
+            "file_metadata checksum 0x{checksum:04x} computed checksum 0x{computed_checksum:04x} "
+        );
+        if checksum == computed_checksum {
+            log::debug!("checksum match");
+        } else {
+            log::error!("fail checksum");
+            return Err(nom::Err::Error(BlockError::Checksum(format!(
+                "file_metadata: checksum mismatch 0x{checksum:04x} computed 0x{computed_checksum:04x}"
+            ))));
+        }
+    }
+
+    Ok((remain, fm))
 }
