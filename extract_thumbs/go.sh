c8452a4991f5b77a88e5d685bea09a6fd4e5150f
martinfrances107 <martinfrances107@hotmail.com>
2025-04-13 07:17:15 +0100

    Minor: extract thumbs updated performance comment.

--- extract_thumbs/go.sh
+++ extract_thumbs/go.sh
@@ -2,6 +2,6 @@ rm *.qoi
 rm *.jpg
 rm *.png
 
-# Performance: This takes 1m 20s to render!!!!
+# Performance: This takes 0.078s to render.
 time cargo run --release -- ../assets/gear-holder-print-in-place_04n_022mm_pla_mk4_6h49m.bgcode
 
