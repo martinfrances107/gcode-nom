1cdad099bd113e6d90d3559e54d8b25e2d3b16d4
martinfrances107 <martinfrances107@hotmail.com>
2025-06-08 16:49:29 +0100

    gcode2obj/go.sh Added test pattern to the suite of obj files.

--- gcode2obj/go.sh
+++ gcode2obj/go.sh
@@ -11,4 +11,10 @@ time cargo run --release --  < ../assets/mini_cube_a.gcode > mini_cube_a.obj
 time cargo run --release -- ../assets/both\ parts.bgcode > both.obj
 time cargo run --release -- ../assets/mini_cube_b.bgcode > mini_cube_b.obj
 time cargo run --release -- ../assets/benchy2-mk4s.bgcode > benchy2-mk4s.obj
+# Rounded corners
+time cargo run --  ../assets/g3_box_rounded_anticlockwise.gcode > g3_box_rounded_anticlockwise.obj
+# nibble - a corner cut out.
+time cargo run -- ../assets/g2_box_nibble_clockwise.gcode > g2_box_nibble_clockwise.obj
+# mix of clockwise and anti-clockwise
+time cargo run -- ../assets/arc_demo.gcode > arc_demo.obj
 time cargo run --release -- ../assets/gear-holder-print-in-place_04n_022mm_pla_mk4_6h49m.bgcode > gear-holder.obj
